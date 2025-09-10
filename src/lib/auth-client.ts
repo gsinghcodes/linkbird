5c020e4fbb45b1f35ad0c86a2653f9d6faaffe0c
gsinghcodes <g.singghhh@gmail.com>
2025-09-10 17:57:14 +0530

    fixed auth

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,7 +3,6 @@ import { nextCookies } from "better-auth/next-js";
 
 
 export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000",
+    baseURL: "https://linkbird-eight.vercel.app/",
     plugins: [nextCookies()]
 })
